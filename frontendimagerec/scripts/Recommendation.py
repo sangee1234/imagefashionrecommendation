aa864e988a2fba84acbf7d402c1f692b5f516eb6
sangee1234 <31073660+sangee1234@users.noreply.github.com>
2023-03-13 10:36:31 -0700

    Update Recommendation.py

--- frontendimagerec/scripts/Recommendation.py
+++ frontendimagerec/scripts/Recommendation.py
@@ -1,9 +1,23 @@
-from pyexpat import features
+
 from PIL import Image
 import pandas as pd
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
+import argparse
+import os
+import platform
+import sys
+import numpy as np
+from pathlib import Path
+import torchvision.models as models
+from torchvision.models._utils import IntermediateLayerGetter
+import torch
+from PIL import Image
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, smart_inference_mode
+
 
 #from scipy.spatial.distance import cdist
 # from keras.applications import vgg16
@@ -11,9 +25,8 @@ setup_logger()
 # from keras.applications.imagenet_utils import preprocess_input
 import os
 from pathlib import Path
-
+import cv2
 import numpy as np
-
 import torch
 # import tensorflow as tf
 # from tensorflow.keras.utils import load_img, img_to_array
@@ -22,51 +35,236 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 import torchvision.models as models
 from torchvision.models._utils import IntermediateLayerGetter
+from torchvision import transforms
+
 
 def get_similar_images(category, input_image):
-  # vgg_model = vgg16.VGG16(weights='imagenet')
-  # feat_extractor = Model(inputs=vgg_model.input, outputs=vgg_model.get_layer("fc2").output)
-  # img = input_image.resize((224,224))
-  # img_arr = img_to_array(img)
-  # img_dim = np.expand_dims(img_arr, axis=0)
-  # processed_img = preprocess_input(img_dim.copy())
+  features = None
   try:
     vgg_model = models.vgg19(pretrained=True)
     torch.save(vgg_model.state_dict(),"model_vgg.pth")
-    # model = models.vgg16(pretrained=False)
-    # if (dict_path != None):
-    #     model.load_state_dict(torch.load(dict_path))
-    # return_layers = {'15': 'out_layer15'}
-    # model_with_multuple_layer = IntermediateLayerGetter(vgg_model.features, return_layers=return_layers)
-    # features = model_with_multuple_layer(torch.from_numpy(input_image))
-    features = vgg_model(torch.from_numpy(input_image))
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0) 
+    features = vgg_model(input_batch)
 
   except Exception as e:
     print(e)
 
-  features = vgg_model(input_image)
-
-
-  source_features = feat_extractor.predict(processed_img)
-  df = pd.read_csv("namazon_"+category+".csv")
-  df[features] = features
- 
-  df[dist] =  cdist(df.features, source_features, metric="cosine")
+  df = pd.read_csv("features_"+category+".csv")
+  df['dist'] =  cdist(df.features, features, metric="cosine")
   df_sorted = df.sort_values(by=['dist'])
-  
 
-def get_recommendations(input_image):
-    cfg2 = get_cfg()
-    cfg2.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-    # model_path = os.path('../../Downloads/model_final.pth')
-    model_path = str(os.path.join(Path.home(), "Downloads/model_final.pth"))
-    cfg2.MODEL.WEIGHTS = model_path
-    cfg2.MODEL.DEVICE = 'cpu'
-    predictor = DefaultPredictor(cfg2)
-    outputs = predictor(input_image) 
-    classes = outputs["instances"].pred_classes
-    boxes = outputs["instances"].pred_boxes
-    for idx, b in enumerate(boxes):
-        cropped_img = img.crop(b)
-        get_similar_images(classes[idx], cropped_img)
+# def get_similar_images(category, input_image):
+#   # vgg_model = vgg16.VGG16(weights='imagenet')
+#   # feat_extractor = Model(inputs=vgg_model.input, outputs=vgg_model.get_layer("fc2").output)
+#   # img = input_image.resize((224,224))
+#   # img_arr = img_to_array(img)
+#   # img_dim = np.expand_dims(img_arr, axis=0)
+#   # processed_img = preprocess_input(img_dim.copy())
+#   try:
+#     vgg_model = models.vgg19(pretrained=True)
+#     torch.save(vgg_model.state_dict(),"model_vgg.pth")
+#     # model = models.vgg16(pretrained=False)
+#     # if (dict_path != None):
+#     #     model.load_state_dict(torch.load(dict_path))
+#     # return_layers = {'15': 'out_layer15'}
+#     # model_with_multuple_layer = IntermediateLayerGetter(vgg_model.features, return_layers=return_layers)
+#     # features = model_with_multuple_layer(torch.from_numpy(input_image))
+#     features = vgg_model(torch.from_numpy(input_image))
+
+#   except Exception as e:
+#     print(e)
+
+#   print("i...")
+#   #features = vgg_model(input_image)
+
+
+#   # source_features = feat_extractor.predict(processed_img)
+#   # df = pd.read_csv("namazon_"+category+".csv")
+#   # df[features] = features
+#   '''
+#   df[dist] =  cdist(df.features, source_features, metric="cosine")
+#   df_sorted = df.sort_values(by=['dist'])
+#   '''
+
+@smart_inference_mode()
+def run(
+        weights='best.pt',  # model path or triton URL
+        source=None,  # file/dir/URL/glob/screen/0(webcam)
+        data='.',  # dataset.yaml path
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img=False,  # show results
+        save_txt=False,  # save results to *.txt
+        save_conf=False,  # save confidences in --save-txt labels
+        save_crop=False,  # save cropped prediction boxes
+        nosave=False,  # do not save images/videos
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        project= 'runs/detect',  # save results to project/name
+        name='exp',  # save results to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        vid_stride=1,  # video frame-rate stride
+):
+    print("2.......")
+    source = str(source)
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
+    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+    webcam = source.isnumeric() or source.endswith('.streams') or (is_url and not is_file)
+    screenshot = source.lower().startswith('screen')
+    if is_url and is_file:
+        source = check_file(source)  # download
+
+    # Directories
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    # Dataloader
+    bs = 1  # batch_size
+    if webcam:
+        view_img = check_imshow(warn=True)
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+        bs = len(dataset)
+    elif screenshot:
+        dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+    vid_path, vid_writer = [None] * bs, [None] * bs
+    # Run inference
+    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    for path, im, im0s, vid_cap, s in dataset:
+        with dt[0]:
+            im = torch.from_numpy(im).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+
+        # Inference
+        with dt[1]:
+            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = model(im, augment=augment, visualize=visualize)
+
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            if webcam:  # batch_size >= 1
+                p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                s += f'{i}: '
+            else:
+                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # im.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+            s += '%gx%g ' % im.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            imc = im0.copy() if save_crop else im0  # for save_crop
+            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                        with open(f'{txt_path}.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or save_crop or view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                    if save_crop:
+                        save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Stream results
+            im0 = annotator.result()
+            if view_img:
+                if platform.system() == 'Linux' and p not in windows:
+                    windows.append(p)
+                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                else:  # 'video' or 'stream'
+                    if vid_path[i] != save_path:  # new video
+                        vid_path[i] = save_path
+                        if isinstance(vid_writer[i], cv2.VideoWriter):
+                            vid_writer[i].release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                        save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
+                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer[i].write(im0)
+
+        # Print time (inference-only)
+        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+
+    # Print results
+    t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
+    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
+    if update:
+        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
+    return str(save_dir / 'crops')
+
+
+def get_recommendations():
+    print("1.......")
+    res_directory = run(save_crop=True, source="test_image.jpg")
+    categories = os.listdir(res_directory)
+    for category in categories:
+        print(category)
+        images = os.listdir(res_directory+"/"+category)
+        get_similar_images(category, Image.open(images[0]))
 #   img = load_img(input_image_path, target_size=(224, 224))
